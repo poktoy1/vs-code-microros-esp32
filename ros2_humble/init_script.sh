9f254782a8692c72d3a2421ed5ff567d42381705
poktoy <poktoy@gmail.com>
2022-09-01 23:11:17 +0400

    mkdir src in ros2 workspace

--- ros2_humble/init_script.sh
+++ ros2_humble/init_script.sh
@@ -1,8 +1,9 @@
 #/bin/bash
+mkdir -p ./src
 vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
 sudo apt upgrade
 sudo rosdep init
 rosdep update
-mkdir -p src
+
 rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
 colcon build --symlink-install -y
