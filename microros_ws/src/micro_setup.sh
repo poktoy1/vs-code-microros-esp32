7ae07e029861edacb7ccd58c106bcb325197f390
poktoy <poktoy@gmail.com>
2022-09-01 22:48:51 +0400

    initial commit

---
+++ microros_ws/src/micro_setup.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+git clone -b $ROS_DISTRO https://github.com/micro-ROS/micro_ros_setup.git src/micro_ros_setup
+sudo apt update && rosdep update
+rosdep install --from-paths src --ignore-src -y
+sudo apt install python3-pip -y
+colcon build
+source install/local_setup.bash
+# Create firmware step
+ros2 run micro_ros_setup create_firmware_ws.sh host
+# Build step
+ros2 run micro_ros_setup build_firmware.sh
+source install/local_setup.bash
+# Download micro-ROS-Agent packages
+ros2 run micro_ros_setup create_agent_ws.sh
+# Build step
+ros2 run micro_ros_setup build_agent.sh
+source install/local_setup.bash
---
+++ ros2_humble/src/init_script.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
+sudo apt upgrade
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
+colcon build --symlink-install -y
