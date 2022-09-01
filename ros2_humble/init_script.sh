85f03fe9c78aa2a02d97f2267ba2c3711682fd8d
poktoy <poktoy@gmail.com>
2022-09-01 23:14:25 +0400

    added apt update

--- ros2_humble/init_script.sh
+++ ros2_humble/init_script.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 mkdir -p ./src
 vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
-sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 sudo rosdep init
 rosdep update
 
